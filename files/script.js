fa45721fb357e063b6786d70fe35744231aff7eb
Dani-8 <daniyalw6190@gmail.com>
2025-08-13 01:31:11 +0500

    updated

--- files/script.js
+++ files/script.js
@@ -1,92 +1,55 @@
-const loginForm = document.getElementById('loginForm');
+const regForm = document.getElementById('regForm');
 const storeDataDiv = document.getElementById('storeData');
+const clearBTN = document.getElementById('clearHistory');
 
-
-const loginHistory = JSON.parse(localStorage.getItem("loginHistory")) || [];
+const regHistory = JSON.parse(localStorage.getItem("regHistory")) || [];
 
 
 function renderHistory(){
-    if(loginHistory.length === 0){
-        storeDataDiv.innerHtml = "<p>No login history available.</p>";
+    if(regHistory.length === 0){
+        storeDataDiv.innerHTML = "<p>No Registration History Yet....</p>";
+
+        clearBTN.style.display = "none"
         return
     }
 
-    storeDataDiv.innerHTML = "<strong>Login History:</strong> <br>" +
-        
+    storeDataDiv.innerHTML = "<strong>Registration History:</strong><br>——————————<br>" +
+        regHistory.map((entry, index) => 
+            `${index+1}. <strong>${entry.name}</strong> --- ${entry.email}`
+        ).join('<br>');
 
+        clearBTN.style.display = "block"
 
 }
 
+renderHistory()
 
-
-
-
-
-loginForm.addEventListener('submit', (e) => {
+regForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const email = document.getElementById('email').value.trim()
-    const password = document.getElementById('password').value.trim()
+    const name = document.getElementById('name').value.trim()
+
+    if(email && name){
+        const entry = { email, name}
+        regHistory.push(entry);
+
+        localStorage.setItem("regHistory", JSON.stringify(regHistory))
+        renderHistory()
 
 
-
-
+        alert(`Welcome, ${name}`)
+        regForm.reset();
+    }
 })
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+clearBTN.addEventListener('click', () => {
+    localStorage.removeItem("regHistory")
+    regHistory.length = 0
+    renderHistory()
+})
 
 
