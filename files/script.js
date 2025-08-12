8035fc20fd0f0e840aad90fa64e29e7aafc9ab1e
Dani-8 <daniyalw6190@gmail.com>
2025-08-12 23:58:06 +0500

    minor updates

--- files/script.js
+++ files/script.js
@@ -1,3 +1,39 @@
+const loginForm = document.getElementById('loginForm');
+const storeDataDiv = document.getElementById('storeData');
+
+
+const loginHistory = JSON.parse(localStorage.getItem("loginHistory")) || [];
+
+
+function renderHistory(){
+    if(loginHistory.length === 0){
+        storeDataDiv.innerHtml = "<p>No login history available.</p>";
+        return
+    }
+
+    storeDataDiv.innerHTML = "<strong>Login History:</strong> <br>" +
+        
+
+
+}
+
+
+
+
+
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value.trim()
+    const password = document.getElementById('password').value.trim()
+
+
+
+
+})
+
+
 
 
 
